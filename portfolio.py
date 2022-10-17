3bc98d62ff3067335964e7c68eaa412102a28041
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-10-17 10:10:51 -0300

    teste multpage app

---
+++ portfolio.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.markdown("# Home🎈")
+st.sidebar.markdown("# Home 🎈")
+
+# Contents of ~/my_app/pages/page_2.py
+import streamlit as st
+
+st.markdown("# Sobre ❄️")
+st.sidebar.markdown("# Sobre ❄️")
