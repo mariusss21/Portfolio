ed55cabae3f86d039b8e479c8c8e3ad3dd930863
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-12-20 09:14:47 -0300

    trabalhando o sobre

--- 01_Sobre.py
+++ 01_Sobre.py
@@ -37,3 +37,12 @@ div.streamlit-expanderHeader{
 
 # Página sobre
 st.markdown("# Sobre")
+
+st.markdown("""
+Engenheiro de formação, trabalhei por 4 anos em indústrias de diversos segmentos, sendo a programação de PLC e desenvolvimento de soluções IoT as minhas principais atribuições. Em 2021 comecei a trabalhar com dados (gerando, coletando, tratando e exibindo) utilizando Python como principal ferramenta. 
+
+Trabalhar em várias fábricas, com equipamentos, máquinas e processos distintos me ajudou a desenvolver um perfil generalista e grande capacidade de resolver problemas. Também gosto bastante de aprender e integar novas tecnologias as soluções e desenvolvi um apreço particular por dados. 
+
+Um caso interessante foi quando fui a um cliente para uma assistência técnica em uma máquina que não conhecia previamente e que o código estava em algumas linguagens diferentes (Ladder, Texto estruturado e SFC) e escrito em 4 línguas diferentes (português, inglês, alemão e italiano). Foi complicado no início, mas o problema foi resolvido. 
+
+Um pouco da minha formação: Engenheiro Eletrônico pela Universidade Federal de Pernambuco. Bolsista do programa Ciência sem Fronteiras do Governo Federal, intercâmbio realizado no departamento de Engenharia Eletrônica da Dublin City University.""")
